d603e7a5d5d45347ef7c94aac99729d0309327be
Stephen Lenane <slenane1@gmail.com>
2025-04-26 14:12:33 +0200

    Updated file locations

--- core/google_integration.py
+++ core/google_integration.py
@@ -10,21 +10,23 @@ from googleapiclient.discovery import build
 
 # If modifying these SCOPES, delete the file token.json
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+credentials_path = '../credentials.json'
+token_path = '../token.json'
 
 def get_todays_events():
     creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists(token_path):
+        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
 
     # If there are no valid credentials, let the user log in'
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token: 
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
             creds = flow.run_local_server(port=0)
 
-        with open('token.json', 'w') as token:
+        with open(token_path, 'w') as token:
             token.write(creds.to_json())
     
     service = build('calendar', 'v3', credentials=creds)
